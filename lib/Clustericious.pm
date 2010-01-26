fb8ed8c8a6bbc483ff3f5165b4f28bde098bc7f5
Brian Duggan <bduggan@matatu.org>
2010-01-26 16:02:33 -0500

    new top level file, and log tweaks

---
+++ lib/Clustericious.pm
@@ -0,0 +1,12 @@
+package Clustericious;
+
+=head1 NAME
+
+Clustericious -- mojolicious based cluster processing system
+
+=cut
+
+our $VERSION = '0.01';
+
+1;
+
--- lib/Clustericious/Node.pm
+++ lib/Clustericious/Node.pm
@@ -2,18 +2,29 @@ package Clustericious::Node;
 
 use base 'Mojolicious';
 use MojoX::Log::Log4perl;
+use Sys::Hostname qw/hostname/;
 
 sub startup {
     my $self = shift;
 
-    # Routes
+    # Logging
+    $ENV{LOG_LEVEL} ||= ( $ENV{HARNESS_ACTIVE} ? "WARN" : "DEBUG" );
+
+    # NB: allow a conf file
+    my $logger = MojoX::Log::Log4perl->new( {
+      "log4perl.rootLogger"               => "$ENV{LOG_LEVEL}, SCREEN",
+      "log4perl.appender.SCREEN"          => "Log::Log4perl::Appender::Screen",
+      "log4perl.appender.SCREEN.layout"   => "PatternLayout",
+      "log4perl.appender.SCREEN.layout.ConversionPattern" => "[%d] [%H:%P] [%5p] %m%n",
+      # %c will show the category, to add things like :
+      # "log4perl.logger.Mojolicious.Plugin.RequestTimer" => "ERROR"
+    });
+    $self->log( $logger );
+
+    my $host = hostname;
+    $self->log->info("Initialized logger for $host $$ to level ".$self->log->level);
+
     my $r = $self->routes;
-
-    # Default route
-    $self->log( MojoX::Log::Log4perl->new() );
-
-    $self->log->trace("Initialized logger");
-
     Clustericious::RouteManager->add_routes($r);
 
 }
