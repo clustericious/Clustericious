625009ca77dd7f197262f6c877174926e12ae115
Brian Duggan <bduggan@matatu.org>
2010-01-28 15:09:49 -0500

    nother test, changed logging

--- lib/Clustericious/Node.pm
+++ lib/Clustericious/Node.pm
@@ -2,7 +2,6 @@ package Clustericious::Node;
 
 use base 'Mojolicious';
 use MojoX::Log::Log4perl;
-use Sys::Hostname qw/hostname/;
 
 sub startup {
     my $self = shift;
@@ -10,19 +9,25 @@ sub startup {
     # Logging
     $ENV{LOG_LEVEL} ||= ( $ENV{HARNESS_ACTIVE} ? "WARN" : "DEBUG" );
 
+    my $name = ref $self;
+
     # NB: allow a conf file
     my $logger = MojoX::Log::Log4perl->new( {
       "log4perl.rootLogger"               => "$ENV{LOG_LEVEL}, SCREEN",
       "log4perl.appender.SCREEN"          => "Log::Log4perl::Appender::Screen",
       "log4perl.appender.SCREEN.layout"   => "PatternLayout",
-      "log4perl.appender.SCREEN.layout.ConversionPattern" => "[%d] [%H:%P] [%5p] %m%n",
+      "log4perl.appender.SCREEN.layout.ConversionPattern" => "[%d] [$name %H %P] %5p: %m%n",
       # %c will show the category, to add things like :
-      # "log4perl.logger.Mojolicious.Plugin.RequestTimer" => "ERROR"
+       "log4perl.logger.Mojolicious.Plugin.RequestTimer" => "ERROR",
+      # "log4perl.logger.Mojolicious" => "ERROR",
+       "log4perl.logger.Mojolicious.Plugin.RequestTimer" => "ERROR",
+       "log4perl.logger.MojoX.Dispatcher.Routes" => "ERROR",
+      # "log4perl.logger.Restmd" => "ERROR",
+      # "log4perl.logger.Mojo.Server.Daemon" => "ERROR",
     });
     $self->log( $logger );
 
-    my $host = hostname;
-    $self->log->info("Initialized logger for $host $$ to level ".$self->log->level);
+    $self->log->info("Initialized logger to level ".$self->log->level);
 
     my $r = $self->routes;
     Clustericious::RouteBuilder->add_routes($self);
---
+++ t/node.t
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 3;
+use Test::Mojo;
+
+package SomeService;
+
+use base 'Clustericious::Node';
+use Clustericious::RouteBuilder;
+
+get '/' => sub { shift->render_text("hello"); };
+
+package main;
+
+my $t = Test::Mojo->new(app => "SomeService");
+
+$t->get_ok("/")->status_is(200)->content_like(qr/hello/, "got content");
+
+1;
+
+
