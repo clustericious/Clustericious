0a779a52e956b5380da85f9f95414db8342450d2
Brian Duggan <brian.duggan@nasa.gov>
2010-03-17 14:58:11 -0400

    Clustericious::RouteBuilder::CRUD

---
+++ lib/Clustericious/RouteBuilder/CRUD.pm
@@ -0,0 +1,72 @@
+package Clustericious::RouteBuilder::CRUD;
+use Mojo::JSON;
+use strict;
+
+use Sub::Exporter -setup => {
+    exports => [
+        "create" => \&_build_create,
+        "read"   => \&_build_read,
+        "update" => \&_build_update,
+        "delete" => \&_build_delete,
+    ],
+    collectors => ['defaults'],
+};
+
+sub _build_create {
+    my ($class, $name, $arg, $defaults) = @_;
+    my $model = $arg->{model} || $defaults->{defaults}{model} || die "no model defined";
+    $model->can("lookup_class") or die "$model must be able to lookup_class";
+    return sub {
+        my $self  = shift;
+        $self->app->log->info("called do_create");
+        my $table = $self->stash->{table};
+        my $p = $self->req->headers->content_type eq "application/json"
+              ? Mojo::JSON->new->decode( $self->req->body )
+              : $self->req->params->to_hash;
+        my $object_class = $model->lookup_class($table);
+        my $object = $object_class->new(%$p);
+        $object->save or $self->app->logdie( $object->errors );
+        $self->stash->{json} = $object->as_tree;
+    };
+}
+
+sub _build_read {
+    my ($class, $name, $arg, $defaults) = @_;
+    my $model = $arg->{model} || $defaults->{defaults}{model} || die "no model defined";
+    $model->can("find_object") or die "$model must be able to find_object";
+    sub {
+        my $self  = shift;
+        my $table = $self->stash->{table};
+        my $key   = $self->stash->{key};
+        my $obj   = $model->find_object($table,$key)
+            or return $self->app->static->serve_404($self,"404.html.ep");
+        $self->app->log->debug("Viewing $table $key");
+
+        $self->stash->{json} = $obj->as_tree;
+    };
+}
+
+sub _build_delete {
+    my ($class, $name, $arg, $defaults) = @_;
+    my $model = $arg->{model} || $defaults->{defaults}{model} || die "no model defined";
+    $model->can("find_object") or die "$model must be able to find_object";
+    sub {
+        my $self  = shift;
+        my $table = $self->stash->{table};
+        my @keys = split /\//, $self->stash->{key};
+        my $obj   = $model->find_object($table,@keys)
+            or return $self->app->static->serve_404($self,"404.html.ep");
+        $self->app->log->debug("Deleting $table @keys");
+
+        $obj->delete or $self->app->logdie($obj->errors);
+        $self->stash->{text} = "ok";
+    }
+}
+
+sub _build_update {
+    return sub { die "update not yet implemented"; };
+}
+
+
+1;
+
---
+++ t/crud.t
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 3;
+use Test::Mojo;
+
+package Fake::Object::Thing;
+
+sub new     { my $class = shift; bless { got => {@_} }, $class; }
+sub save    { return 1; }
+sub as_tree { return shift->{got} };
+
+package Fake::Object;
+
+sub lookup_class { return "Fake::Object::Thing"; }
+sub find_object {  return Fake::Object::Thing->new() }
+
+package SomeService;
+
+use base 'Clustericious::App';
+use Clustericious::RouteBuilder;
+use Clustericious::RouteBuilder::CRUD
+        "create" => { -as => "do_create" },
+        defaults => { model => "Fake::Object" };
+
+post '/' => \&do_create;
+
+package main;
+
+my $t = Test::Mojo->new(app => "SomeService");
+
+$t->post_form_ok("/", { foo => "bar" }, "posted to create")
+    ->status_is(200, "got 200")
+    ->json_content_is({foo => "bar"}, "got structure back");
+
+1;
+
+
