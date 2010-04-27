5988d6fb29ac349137fe6eb20afa4c5a7ec4d66a
Brian Duggan <brian.duggan@nasa.gov>
2010-04-27 08:34:35 -0400

    configs

--- lib/Clustericious/App.pm
+++ lib/Clustericious/App.pm
@@ -3,30 +3,48 @@ package Clustericious::App;
 use base 'Mojolicious';
 use List::Util qw/first/;
 use MojoX::Log::Log4perl;
+use warnings;
+use strict;
+
+our @Confdirs;
 
 sub startup {
+    my $self = shift;
+    our @Confdirs = $ENV{TEST_HARNESS} ? 
+        ($ENV{CLUSTERICIOUS_TEST_CONF_DIR}) :
+        ($ENV{HOME}, "$ENV{HOME}/etc", $self->home, $self->home."/etc",
+         "/util/etc", "/etc" );
+
+    $self->_init_logging();
+
+    my $config = $self->_load_config();
+    $self->_load_service_configs($config);
+
+    my $r = $self->routes;
+    Clustericious::RouteBuilder->add_routes($self);
+}
+
+sub _init_logging {
     my $self = shift;
 
     # Logging
     $ENV{LOG_LEVEL} ||= ( $ENV{HARNESS_ACTIVE} ? "WARN" : "DEBUG" );
 
-    my $name = ref $self;
+    my $app_name = lc ref $self;
 
-    my $dir; # dir with log config file.
-    my $pattern; # pattern for screen logging
+    my $l4p_dir; # dir with log config file.
+    my $l4p_pat; # pattern for screen logging
 
     if ($ENV{HARNESS_ACTIVE}) {
-        $pattern = "# %5p: %m%n";
+        $l4p_pat = "# %5p: %m%n";
     } else  {
-        my @dirs =  ($ENV{HOME}, $self->home, $self->home."/etc", );
-        push @dirs, "$_", "$_/etc" if ($_ = $ENV{MOJO_HOME});
-        $dir = first { -e "$_/log4perl.conf" } @dirs;
-        $pattern =  "[%d] [%Z %H %P] %5p: %m%n";
+        $l4p_dir  = first { -d $_ && -e "$_/log4perl.conf"  } @Confdirs;
+        $l4p_pat  = "[%d] [%Z %H %P] %5p: %m%n";
     }
 
-    Log::Log4perl::Layout::PatternLayout::add_global_cspec('Z', sub {$name});
+    Log::Log4perl::Layout::PatternLayout::add_global_cspec('Z', sub {$app_name});
 
-    my $logger = MojoX::Log::Log4perl->new( $dir ? "$dir/log4perl.conf":
+    my $logger = MojoX::Log::Log4perl->new( $l4p_dir ? "$l4p_dir/log4perl.conf":
       { # default config
        ($ENV{LOG_FILE} ? (
           "log4perl.rootLogger"              => "$ENV{LOG_LEVEL}, File1",
@@ -38,7 +56,7 @@ sub startup {
           "log4perl.rootLogger"               => "$ENV{LOG_LEVEL}, SCREEN",
           "log4perl.appender.SCREEN"          => "Log::Log4perl::Appender::Screen",
           "log4perl.appender.SCREEN.layout"   => "PatternLayout",
-          "log4perl.appender.SCREEN.layout.ConversionPattern" => "$pattern",
+          "log4perl.appender.SCREEN.layout.ConversionPattern" => "$l4p_pat",
        )),
       # These categories (%c) are too verbose by default :
        "log4perl.logger.Mojolicious"                     => "WARN",
@@ -48,11 +66,32 @@ sub startup {
     $self->log( $logger );
 
     $self->log->info("Initialized logger to level ".$self->log->level);
-    $self->log->info("Log config found in $dir/log4perl.conf") if $dir;
-    warn "# Using $dir/log4perl.conf for log config\n" if $dir;
+    $self->log->info("Log config found in $l4p_dir/log4perl.conf") if $l4p_dir;
+    warn "# Using $l4p_dir/log4perl.conf for log config\n" if $l4p_dir;
+}
 
-    my $r = $self->routes;
-    Clustericious::RouteBuilder->add_routes($self);
+sub _load_config {
+    my $self = shift;
+    our @Confdirs;
+    my $app_name = $_[0] || lc ref $self;
+    my $stash_key = @_ ? "config_$app_name" : "config";
+    my $conf_dir = first { -d $_ && -e "$_/$app_name.conf" } @Confdirs
+      or die "cannot load config for $app_name";
+    warn "# Using $conf_dir/$app_name.conf for $app_name\n";
+    return $self->plugin( 'json_config',
+        { file => "$conf_dir/$app_name.conf", stash_key => $stash_key } );
+}
+
+sub _load_service_configs {
+    my ($self,$config) = @_;
+    for (@{ $config->{services} }) {
+        next if $_->{url};
+        my $service = $_->{name};
+        $self->_load_config($service);
+    }
+}
+
+sub service {
 
     $self->plugins->namespaces(['Clustericious::Plugin']);
     $self->plugin('data_handler');
