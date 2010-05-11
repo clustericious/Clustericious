5820c0362ac1bd85b52b3b74cc6a6a3aeb917c9a
Curt Tilmes <Curt.Tilmes@nasa.gov>
2010-05-11 13:47:46 -0400

    Get start arguments from config file

--- lib/Clustericious/Commands.pm
+++ lib/Clustericious/Commands.pm
@@ -1,49 +1,33 @@
 package Clustericious::Commands;
-use File::Basename qw/basename/;
+
+use strict;
+use warnings;
+
 use Clustericious::Config;
 
 use base 'Mojolicious::Commands';
-__PACKAGE__->attr(
-    namespaces => sub { [qw/Mojolicious::Command Mojo::Command Clustericious::Command/] });
+
+__PACKAGE__->attr(namespaces => sub { [qw/Clustericious::Command
+                                          Mojolicious::Command
+                                          Mojo::Command/] });
 
 sub start {
    my $self = shift;
 
-   my @args = @_;
+   my @args = @_ ? @_ : @ARGV;
 
-   my $config = Clustericious::Config->new;
-   use Data::Dumper;
-   die Dumper($config);
+   my $config = Clustericious::Config->new($ENV{MOJO_APP});  
 
-   # Given $0, use the config file to find the default args
-   # Then override using @ARGV,
-   # then call self->SUPER::start(@args);
-
-   #  will have to use similar logic to App::_load_config but actually
-   #  parse the json files.
-
-   # TODO : make this a method call somewhere
-   our @Confdirs = $ENV{TEST_HARNESS} ? 
-        ($ENV{CLUSTERICIOUS_TEST_CONF_DIR}) :
-        ($ENV{HOME}, "$ENV{HOME}/etc", $self->home, $self->home."/etc",
-         "/util/etc", "/etc" );
-
-   # TODO: how do we find our $app_name?
-   my $conf_dir = first { -d $_ && -e "$_/$app_name.conf" } @Confdirs
-      or die "cannot load config for $app_name";
-
-   my $conf_data = JSON::XS->new->decode("$conf_dir/$app_name.conf");
-
-   my $command = basename $0;
-   @args = $conf_data->{$command}{args} unless @ARGV;
-   #
-   # The conf file should have, e.g.
-   #daemon_prefork:
-   #    args:[ --listen, <%= $url =%>, --lockfile /tmp/lockfile. ..., -- ]
+   unless (@args)
+   {
+       push(@args, $config->{start_mode});
+       while (my ($key, $value) = each %{$config->{$config->{start_mode}}})
+       {
+           push(@args, "--$key", "$value");
+       }
+   }
 
    $self->SUPER::start(@args);
 }
 
-
 1;
-
