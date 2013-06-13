473077ce661151b0f66815670cf9e66bddae4a88
Graham Ollis <gollis@sesda2.com>
2013-06-13 07:39:46 -0400

    rm skip_auth

--- lib/Clustericious/Plugin/PlugAuth.pm
+++ lib/Clustericious/Plugin/PlugAuth.pm
@@ -84,7 +84,7 @@ sub register {
         WARN "unable to determine PlugAuth URL: $@";
         return $self;
     }
-    $app->hook(after_dispatch => sub { Clustericious::Plugin::PlugAuth->skip_auth(0) });
+    #$app->hook(after_dispatch => sub { Clustericious::Plugin::PlugAuth->skip_auth(0) });
     $self;
 }
 
@@ -101,7 +101,7 @@ sub authenticate {
     my $self = shift;
     my $c = shift;
     my $realm = shift;
-    return 1 if $self->skip_auth;
+    #return 1 if $self->skip_auth;
 
     TRACE ("Authenticating for realm $realm");
     # Everyone needs to send an authorization header
@@ -188,7 +188,7 @@ the given action on the given resource.
 sub authorize {
     my $self = shift;
     my $c = shift;
-    return 1 if $self->skip_auth;
+    #return 1 if $self->skip_auth;
     my ($action,$resource) = @_;
     my $user = $c->stash("user") or LOGDIE "missing user in authorize()";
     LOGDIE "missing action or resource in authorize()" unless @_==2;
@@ -207,22 +207,22 @@ sub authorize {
     return 0;
 }
 
-=head2 skip_auth
+#=head2 skip_auth
+#
+# Clustericious::Plugin::PlugAuth->skip_auth(1);
+#
+#Set this global flag to bypass authentication and authorization, e.g. during
+#a subequest.  This flag is reset at the end of the dispatch cycle.
+#
+#=cut
 
- Clustericious::Plugin::PlugAuth->skip_auth(1);
-
-Set this global flag to bypass authentication and authorization, e.g. during
-a subequest.  This flag is reset at the end of the dispatch cycle.
-
-=cut
-
-sub skip_auth {
-    state $skip_auth = 0;
-    my $class = shift;
-    return $skip_auth unless @_;
-    $skip_auth = shift;
-    return $skip_auth;
-}
+#sub skip_auth {
+#    state $skip_auth = 0;
+#    my $class = shift;
+#    return $skip_auth unless @_;
+#    $skip_auth = shift;
+#    return $skip_auth;
+#}
 
 =head1 SEE ALSO
 
