cd16b8541192e83a7237727ec2c1178b40822257
Graham Ollis <perl@wdlabs.com>
2015-11-17 22:31:54 -0500

    modern perl!

--- lib/Clustericious/Config/Password.pm
+++ lib/Clustericious/Config/Password.pm
@@ -1,8 +1,8 @@
 package Clustericious::Config::Password;
 
-use Data::Dumper;
 use strict;
 use warnings;
+use 5.010;
 
 # ABSTRACT: Password routines for Clustericious::Config
 # VERSION
@@ -24,17 +24,15 @@ L<Clustericious::Config>, L<Clustericious>
 
 =cut
 
-our $Stashed;
-
 sub sentinel {
     return "__XXX_placeholder_ceaa5b9c080d69ccdaef9f81bf792341__";
 }
 
 sub get {
     my $self = shift;
-    require Term::Prompt;
-    $Stashed ||= Term::Prompt::prompt('p', 'Password:', '', '');
-    $Stashed;
+    state $pass;
+    $pass //= do { require Term::Prompt; Term::Prompt::prompt('p', 'Password:', '', '') };
+    $pass;
 }
 
 sub is_sentinel {
