fa58c2748603e14202a9075bb8e1fadb8f8b5076
Graham Ollis <perl@wdlabs.com>
2015-11-18 08:25:26 -0500

    add deprecation and removal message

--- lib/Clustericious/Config/Plugin.pm
+++ lib/Clustericious/Config/Plugin.pm
@@ -3,6 +3,7 @@ package Clustericious::Config::Plugin;
 use strict;
 use warnings;
 use Clustericious::Config::Helpers;
+use Carp ();
 
 # ABSTRACT: Deprecated module
 # VERSION
@@ -21,5 +22,13 @@ sub Clustericious::Config::Helpers::cluster
 {
   goto &Clustericious::Config::Plugin::cluster;
 }
+
+Carp::carp "Clustericious::Config::Plugin is deprecated and will be removed on or after January 31 2016";
+
+if(scalar caller eq 'Test::Clustericious::Cluster')
+{
+  Carp::carp "Upgrade your version of Test::Clustericious::Cluster, otherwise it will stop working when Clustericious::Config::Plugin is removed";
+}
+
 
 1;
