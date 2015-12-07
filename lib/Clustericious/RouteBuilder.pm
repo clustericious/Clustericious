260948b30d94665797be1360fb4dd85693aa80a0
Graham Ollis <plicease@cpan.org>
2015-12-07 14:53:49 -0500

    refactor

--- lib/Clustericious/RouteBuilder.pm
+++ lib/Clustericious/RouteBuilder.pm
@@ -74,26 +74,32 @@ L<Clustericious>, L<Mojolicious::Lite>
 
 =cut
 
-our %Routes;
+my %routes;
 
 # Much of the code below taken directly from Mojolicious::Lite.
 sub import {
-    my $class = shift;
+    my($class, $app_class) = @_;
     my $caller = caller;
-    my $app_class;
-    if (@_) { # allow specification in the "use".
-        $app_class = shift;
-    } elsif ($caller->isa("Clustericious::App")) {
+    
+    unless($app_class)
+    {
+      if ($caller->isa("Clustericious::App"))
+      {
         $app_class = $caller;
-    } else {
+      }
+      else
+      {
         $app_class = $caller;
         $app_class =~ s/::Routes$// or die "could not guess app class : ";
+      }
     }
+
     my @routes;
-    $Routes{$app_class} = \@routes;
+    $routes{$app_class} = \@routes;
 
     # Route generator
-    my $route_sub = sub {
+    my $route_sub = sub
+    {
         my ($methods, @args) = @_;
 
         my ($cb, $constraints, $defaults, $name, $pattern);
@@ -101,31 +107,51 @@ sub import {
 
         # Route information
         my $condition;
-        while (my $arg = shift @args) {
+        while (my $arg = shift @args)
+        {
 
             # Condition can be everything
-            if ($condition) {
+            if ($condition)
+            {
                 push @$conditions, $condition => $arg;
                 $condition = undef;
             }
 
             # First scalar is the pattern
-            elsif (!ref $arg && !$pattern) { $pattern = $arg }
+            elsif (!ref $arg && !$pattern)
+            {
+              $pattern = $arg;
+            }
 
             # Scalar
-            elsif (!ref $arg && @args) { $condition = $arg }
+            elsif (!ref $arg && @args)
+            {
+              $condition = $arg;
+            }
 
             # Last scalar is the route name
-            elsif (!ref $arg) { $name = $arg }
+            elsif (!ref $arg)
+            {
+              $name = $arg;
+            }
 
             # Callback
-            elsif (ref $arg eq 'CODE') { $cb = $arg }
+            elsif (ref $arg eq 'CODE')
+            {
+              $cb = $arg;
+            }
 
             # Constraints
-            elsif (ref $arg eq 'ARRAY') { $constraints = $arg }
+            elsif (ref $arg eq 'ARRAY')
+            {
+              $constraints = $arg;
+            }
 
             # Defaults
-            elsif (ref $arg eq 'HASH') { $defaults = $arg }
+            elsif (ref $arg eq 'HASH')
+            {
+              $defaults = $arg;
+            }
         }
 
         # Defaults
@@ -150,42 +176,35 @@ sub import {
 
     };
 
+    # Export
     monkey_patch $app_class, startup_route_builder => \&_startup_route_builder;
 
-    # Prepare exports
-    no strict 'refs';
-    no warnings 'redefine';
+    monkey_patch $caller, any => sub { $route_sub->(ref $_[0] ? shift : [], @_) };
+    monkey_patch $caller, $_  => sub { unshift @_, 'delete'; goto $route_sub } for qw( Delete del );
 
-    # Export
-    *{"${caller}::any"}          = sub { $route_sub->(ref $_[0] ? shift : [], @_) };
-    *{"${caller}::get"}          = sub { $route_sub->('get', @_) };
-    *{"${caller}::head"}         = sub { $route_sub->('head', @_) };
-    *{"${caller}::ladder"}       = sub { $route_sub->('ladder', @_) };
-    *{"${caller}::post"}         = sub { $route_sub->('post', @_) };
-    *{"${caller}::put"}          = sub { $route_sub->('put', @_) };
-    *{"${caller}::Delete"}       = sub { $route_sub->('delete', @_) };
-    *{"${caller}::del"}          = sub { $route_sub->('delete', @_) };
-    *{"${caller}::websocket"}    = sub { $route_sub->('websocket', @_) };
-    *{"${caller}::authenticate"} = sub { $route_sub->('authenticate',@_) };
-    *{"${caller}::authorize"}    = sub { $route_sub->('authorize',@_) };
+    foreach my $method (qw( get head ladder post put websocket authenticate authorize ))
+    {
+      monkey_patch $caller, $method => sub { unshift @_, $method; goto $route_sub };
+    }
 }
 
 sub _startup_route_builder {
-    my $app = shift;
-    my $auth_plugin = shift;
+    my($app, $auth_plugin) = @_;
 
-    my $stashed = $Routes{ ref $app } // do { WARN "no routes stashed for $app"; [] };
+    my $stashed = $routes{ ref $app } // do { WARN "no routes stashed for $app"; [] };
     my @stashed            = @$stashed;
     my $routes             = $app->routes;
     my $head_route         = $app->routes;
     my $head_authenticated = $head_route;
 
-    for my $spec (@stashed) {
+    for my $spec (@stashed)
+    {
        my      ($name,$pattern,$constraints,$conditions,$defaults,$methods) =
        @$spec{qw/name  pattern  constraints  conditions  defaults  methods/};
 
          # authenticate, always connects to app->routes
-         if (!ref $methods && $methods eq 'authenticate') {
+         if (!ref $methods && $methods eq 'authenticate')
+         {
              my $realm = $pattern || ref $app;
              my $cb = defined $auth_plugin ? sub { $auth_plugin->authenticate(shift, $realm) } : sub { 1 };
              $head_route = $head_authenticated = $routes =
@@ -194,7 +213,8 @@ sub _startup_route_builder {
          }
 
          # authorize replaces previous authorize's
-         if (!ref $methods && $methods eq 'authorize') {
+         if (!ref $methods && $methods eq 'authorize')
+         {
             die "put authenticate before authorize" unless $head_authenticated;
             my $action = $pattern;
             my $resource = $name;
@@ -220,7 +240,8 @@ sub _startup_route_builder {
          }
 
          # ladders don't replace previous ladders
-         if (!ref $methods && $methods eq 'ladder') {
+         if (!ref $methods && $methods eq 'ladder')
+         {
               die "constraints not handled in ladders" if $constraints && @$constraints;
               $routes = $routes->under( $pattern )->over($conditions)
                   ->to($defaults)->name($name);
