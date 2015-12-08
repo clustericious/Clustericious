e4c42067c46be2ccdf52cc01e05f509140a2c904
Graham Ollis <plicease@cpan.org>
2015-12-08 13:27:12 -0500

    add which command

---
+++ lib/Clustericious/Command/which.pm
@@ -0,0 +1,62 @@
+package Clustericious::Command::which;
+
+use strict;
+use warnings;
+use 5.010;
+use Sub::Identify qw( get_code_location get_code_info );
+use Mojo::Base qw( Clustericious::Command );
+
+# ABSTRACT: Clustericious command to start a Clustericious application
+# VERSION 
+
+has description => <<EOT;
+Determine the location of method or helper.
+EOT
+
+has usage => <<EOT;
+usage $0: which method
+EOT
+
+sub run
+{
+  my($self, $method) = @_;
+  my $app = $self->app;
+  my $controller = $app->build_controller;
+  
+  unless($method)
+  {
+    say STDERR "no method specified";
+    exit 1;
+  }
+  
+  my $type;
+  my $sub;
+  
+  if($sub = $controller->can($method))
+  {
+    $type = "controller method";
+  }
+  elsif($sub = $app->renderer->get_helper($method))
+  {
+    $type = "helper";
+  }
+  elsif($sub = $app->can($method))
+  {
+    $type = "app method";
+  }
+  else
+  {
+    say STDERR "No such method or helper: $method";
+    exit 2;
+  }
+
+  my($class, $name) = get_code_info     $sub;
+  my($file,  $line) = get_code_location $sub;
+  
+  say "type:     $type";
+  say "class:    $class";
+  say "name:     $name" if $name ne '__ANON__';
+  say "location: $file:$line";
+}
+
+1;
